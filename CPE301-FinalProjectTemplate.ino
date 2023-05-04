780cf5797bc604326409f21386573ba0663413d3
rockyroadgalaxy <77945946+1001-Lykins-Rachel@users.noreply.github.com>
2023-05-04 12:28:53 -0700

    Update CPE301-FinalProjectTemplate.ino

--- CPE301-FinalProjectTemplate.ino
+++ CPE301-FinalProjectTemplate.ino
@@ -7,10 +7,24 @@ int waterlevel;
 
 void setup (){
   U0init(9600);
+  
+ volatile unsigned char* port_e = (unsigned char*) 0x2E; 
+volatile unsigned char* ddr_e  = (unsigned char*) 0x2D; 
+volatile unsigned char* pin_e  = (unsigned char*) 0x2C;
+  
+  
 }
 
 void loop (){
 /*
+Serial.begin(9600); //set baud rate
+
+*ddr_e |= 0b00001000; //enable pin 5 as output
+*ddr_e |= 0b00000100; //enable pin 4 as output
+*ddr_e |= 0b00000010; //enable pin 3 as output
+
+
+
   if button pressed, then disable system{
     RTC timestamps disabled
     Yellow LED on
@@ -61,11 +75,27 @@ void loop (){
     hum thing
     lcd display both
     if(T_level > threshold){
-      turn on fan
+      //turn on fan
+      *port_e |= 0b00001000; //set pin 5 to HIGH
+      *port_e &= ~(0b00000100); //set pin 4 to LOW
+      *port_e |= 0b00000010;
+  
+      delay(1000);
+      *port_e |= ~(0b00000010);
+      delay(1000);
+  
+  
       return(1);
     }
     else{
-      turn off fan
+      //turn off fan
+      *port_e |= 0b00000100; //set pin 5 to HIGH
+      *port_e &= ~(0b00001000); //set pin 4 to LOW
+      *port_e |= 0b00000010;
+  
+      delay(1000);
+      *port_e |= ~(0b00000010);
+      delay(1000);
       return(0);
     }
   }
