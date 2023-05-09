86adb155c4d671fd4e224961e95a85910fd55be0
1101-Hsueh-Alexandra <98076754+1101-Hsueh-Alexandra@users.noreply.github.com>
2023-05-09 01:50:34 -0700

    Update reservoir.ino

--- Pieces/reservoir.ino
+++ Pieces/reservoir.ino
@@ -1,39 +1,143 @@
-// include the library code: 
-#include <LiquidCrystal.h>
-//initialise the library with the numbers of the interface pins 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
+//#define RDA 0x80
+//#define TBE 0x20  
 
-int resval = 0;  // holds the value
-int respin = A5; // sensor pin used
-int ledPinRed = 13;  // red LED pin
-int buttonPin = 2; // button pin
-int buttonState = 0; // variable for reading the button status
-  
-void setup() { 
-  // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
-  pinMode(resval, INPUT);
-  pinMode(ledPinRed, OUTPUT); 
-} 
-  
-void loop() { 
-  // set the cursor to column 0, line 2 
-  lcd.setCursor(0, 2);   
-  resval = analogRead(respin); //Read data from analog pin and store it to resval variable 
-  if (resval<=100){ 
-    lcd.println("Water level is too low");
-    digitalWrite(ledPinRed, HIGH);
-    digitalWrite(ledPinBlue, LOW);
-    digitalWrite(ledPinGreen, LOW);
-    digitalWrite(ledPinYellow, LOW);
-    *port_e &= ~(0b00001000); //set pin 5 to LOW
-    *port_e &= ~(0b00000100); //set pin 4 to LOW
-    *port_e &= ~(0b00000010); //set pin 3 to LOW
+int RDA = 0x80;
+int TBE = 0x20;
+
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+ 
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
+// Define Port B Register Pointers
+volatile unsigned char* port_f = (unsigned char*) 0x31; 
+volatile unsigned char* ddr_f  = (unsigned char*) 0x30; 
+volatile unsigned char* pin_f  = (unsigned char*) 0x2F; 
+
+void setup() 
+{
+  // setup the UART
+  U0init(9600);
+  // setup the ADC
+  adc_init();
+}
+void loop() 
+{
+  // get the reading from the ADC
+  unsigned int adc_reading = adc_read(0);
+  // print it to the serial port
+  print_int(adc_reading);
+
+  if(adc_reading < 250){
+    Serial.println("Water Level Low");
   }
-  //Get the system back to idle. Temperature and humidity detection do not cease
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH && resval > 100) {
-    digitalWrite(ledPinGreen, HIGH);
-  }  
-  delay(1000); 
+  else{}
+
+}
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b10111111; // clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b10111111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11000000; // clear bit 5 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11011111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00100000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+}
+
+void print_int(unsigned int out_num)
+{
+  // clear a flag (for printing 0's in the middle of numbers)
+  unsigned char print_flag = 0;
+  // if its greater than 1000
+  if(out_num >= 1000)
+  {
+    // get the 1000's digit, add to '0' 
+    U0putchar(out_num / 1000 + '0');
+    // set the print flag
+    print_flag = 1;
+    // mod the out num by 1000
+    out_num = out_num % 1000;
+  }
+  // if its greater than 100 or we've already printed the 1000's
+  if(out_num >= 100 || print_flag)
+  {
+    // get the 100's digit, add to '0'
+    U0putchar(out_num / 100 + '0');
+    // set the print flag
+    print_flag = 1;
+    // mod the output num by 100
+    out_num = out_num % 100;
+  } 
+  // if its greater than 10, or we've already printed the 10's
+  if(out_num >= 10 || print_flag)
+  {
+    U0putchar(out_num / 10 + '0');
+    print_flag = 1;
+    out_num = out_num % 10;
+  } 
+  // always print the last digit (in case it's 0)
+  U0putchar(out_num + '0');
+  // print a newline
+  U0putchar('\n');
+}
+void U0init(int U0baud)
+{
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+unsigned char U0kbhit()
+{
+  return *myUCSR0A & RDA;
+}
+unsigned char U0getchar()
+{
+  return *myUDR0;
+}
+void U0putchar(unsigned char U0pdata)
+{
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
 }
